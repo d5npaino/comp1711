07b94adccef8bf893993f65c72d0c832a9970f89
d5npaino <146723372+d5npaino@users.noreply.github.com>
2023-11-16 16:33:15 +0000

    Bootcamp C Week 7

--- AssessmentTasks/StepsTask1.c
+++ AssessmentTasks/StepsTask1.c
@@ -47,4 +47,33 @@ void tokeniseRecord(const char *input, const char *delimiter,
 // Complete the main function
 int main()
 {
+    char *filename = "FitnessData_2023.csv";
+    FILE *file = fopen(filename, "r");
+    if (file == NULL)
+    {
+        printf("Error opening file\n");
+        return 1;
+    }
+
+    int buffer_size = 500, counter = 0;
+    char line_buffer[buffer_size], steps[100], time[6], date[11];
+    FITNESS_DATA records[500];
+
+    while (fgets(line_buffer, buffer_size, file) != NULL)
+    {
+        tokeniseRecord(line_buffer, ",", date, time, steps);
+        strcpy(records[counter].date, date);
+        strcpy(records[counter].time, time);
+        records[counter].steps = atoi(steps);
+        counter++;
+    }
+
+    printf("Number of records in file: %d\n", counter);
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%s/%s/%d\n", records[i].date, records[i].time, records[i].steps);
+    }
+
+    fclose(file);
+    return 0;
 }
